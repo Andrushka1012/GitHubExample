a269c4230e431906c4fb32cfd523e56ed558851b
Andrii Makarenko <andrii.makarenko@euvic.pl>
2020-07-07 17:01:33 +0200

    fix for glide placeholder

--- app/src/main/java/pl/andrii/githubexample/infrastructure/DataBindingAdapters.kt
+++ app/src/main/java/pl/andrii/githubexample/infrastructure/DataBindingAdapters.kt
@@ -6,15 +6,20 @@ import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import pl.andrii.githubexample.R
 
 @BindingAdapter("android:circularImage")
 fun loadCircular(view: ImageView, value: String?) = value?.let {
+    val factory = DrawableCrossFadeFactory.Builder()
+        .setCrossFadeEnabled(true)
+        .build()
+
    Glide.with(view)
        .load(value)
        .placeholder(R.drawable.ic_repository)
        .circleCrop()
-       .transition(withCrossFade())
+       .transition(withCrossFade(factory))
        .into(view)
 }
 
